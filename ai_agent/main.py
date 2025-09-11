0119d66905603f0c5b72d93928be9e7cfa6f6fa2
sifulsidd <sifulsiddiki@yahoo.com>
2025-09-10 21:45:01 -0400

    Can now run prompts from command line

--- ai_agent/main.py
+++ ai_agent/main.py
@@ -1,15 +1,18 @@
 import os
 from dotenv import load_dotenv
 from google import genai
-
+import sys 
 
 def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
+    # print("Args:", sys.argv[1])
+    
     response = client.models.generate_content(
-        model='gemini-2.0-flash-001', contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
+        model='gemini-2.0-flash-001', 
+        contents=sys.argv[1]
     )
 
     print(response.text)
