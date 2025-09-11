071acfa1bba3024c57f7a926cf3f366d0c2d4257
sifulsidd <sifulsiddiki@yahoo.com>
2025-09-10 21:25:24 -0400

    Created a connection with the Gemini API and made sure we got a response for a prompt

--- ai_agent/main.py
+++ ai_agent/main.py
@@ -1,6 +1,23 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+
 def main():
-    print("Hello from ai-agent!")
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
 
+    response = client.models.generate_content(
+        model='gemini-2.0-flash-001', contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
+    )
 
-if __name__ == "__main__":
-    main()
+    print(response.text)
+    if response is None or response.usage_metadata is None:
+        print("Response is malformed")
+        return
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    # print("API Key:", api_key)
+    
+main()
