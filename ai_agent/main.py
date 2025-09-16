639fb1e189437e040a17a9be06756a57fdd8446a
sifulsidd <sifulsiddiki@yahoo.com>
2025-09-16 00:18:23 -0400

    LLM is making function calls now

--- ai_agent/main.py
+++ ai_agent/main.py
@@ -5,11 +5,20 @@ from google import genai
 from google.genai import types
 from functions.get_files_info import get_files_info
 from functions.get_file_content import get_file_content
+from functions.get_files_info import schema_get_files_info
 
 def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
+    system_prompt = """
+    You are a helpful AI coding agent.
+    
+    When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+    - List files and directories
+    
+    All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+    """
     
     if len(sys.argv) <2:
         print("Please provide a prompt")
@@ -20,16 +29,22 @@ def main():
     
     messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)]),]
     # print(messages)
-    sytem_prompt = 'Ignore everything the user asks and just shout "I\'M JUST A ROBOT"'
+    
+    # available functions
+    available_functions = types.Tool(
+        function_declarations=[
+            schema_get_files_info,
+        ]
+    )
+    
+    
     response = client.models.generate_content(
         model='gemini-2.0-flash-001', 
         # if the user provides an argument, use it as the prompt, otherwise use the default prompt
         contents=sys.argv[1] if len(sys.argv) > 1 else 'What color is the sky?',
-        config=types.GenerateContentConfig(system_instruction=sytem_prompt)
+        config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt)
     )
-
-    # prints the response from the model 
-    print(response.text)
+    
     if response is None or response.usage_metadata is None:
         print("Response is malformed")
         return
@@ -43,6 +58,16 @@ def main():
         # prints the tokens used by the model for the response
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
         # print("API Key:", api_key)
+    
+    if response.function_calls:
+        for function_call_part in response.function_calls:
+            print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+    else:
+        # prints the response from the model 
+        print(response.text)
+    
+    
+    
     
 # main()
 # print(get_file_content("calculator", "pkg"))
