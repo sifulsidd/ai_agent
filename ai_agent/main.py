c33f8b5db23e3edee1a4c10b767a88520d51776d
sifulsidd <sifulsiddiki@yahoo.com>
2025-09-18 12:15:14 -0400

    AI Agent is working as intended

--- ai_agent/main.py
+++ ai_agent/main.py
@@ -47,39 +47,54 @@ def main():
         ]
     )
     
+    max_iterations = 20
     
-    response = client.models.generate_content(
-        model='gemini-2.0-flash-001', 
-        # if the user provides an argument, use it as the prompt, otherwise use the default prompt
-        contents=sys.argv[1] if len(sys.argv) > 1 else 'What color is the sky?',
-        config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt)
-    )
-    
-    if response is None or response.usage_metadata is None:
-        print("Response is malformed")
-        return
-    
-    # check if the user provided the --verbose flag, 
-    # if so, print the user prompt, the prompt tokens, and the response tokens
-    verbose = len(sys.argv) > 2 and sys.argv[2] == "--verbose"
-    
-    if verbose:
-        print("User prompt:", user_prompt)
-        # prints the tokens used by the model
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        # prints the tokens used by the model for the response
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-        # print("API Key:", api_key)
-    
-    
-    if response.function_calls:
-        for function_call_part in response.function_calls:
-            # print(f"Calling function: {function_call_part.name}({function_call_part.args})")
-            result =call_function(function_call_part, verbose)
-            print(result)
-    else:
-        # prints the response from the model 
-        print(response.text)
+    for i in range(0, max_iterations):
+        response = client.models.generate_content(
+            model='gemini-2.0-flash-001', 
+            # if the user provides an argument, use it as the prompt, otherwise use the default prompt
+            # contents=sys.argv[1] if len(sys.argv) > 1 else 'What color is the sky?',
+            # have to update the messages array with the new messages
+            contents=messages,
+            config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt)
+        )
+        
+        # print(response.candidates)
+        
+        if response is None or response.usage_metadata is None:
+            print("Response is malformed")
+            return
+        
+        # check if the user provided the --verbose flag, 
+        # if so, print the user prompt, the prompt tokens, and the response tokens
+        verbose = len(sys.argv) > 2 and sys.argv[2] == "--verbose"
+        
+        if verbose:
+            print(f"User prompt: {user_prompt}")
+            # prints the tokens used by the model
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+            # prints the tokens used by the model for the response
+            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+            # print("API Key:", api_key)
+        
+        # put all the functionas the model wants to call in the array, then we call the messages
+        if response.candidates:
+            for candidate in response.candidates:
+                if candidate is None or candidate.content is None:
+                    continue
+                messages.append(candidate.content)
+        
+                    
+        
+        if response.function_calls:
+            for function_call in response.function_calls:
+                result = call_function(function_call, verbose)
+                messages.append(result)
+        else:
+            # prints the response from the model 
+            # final agent text message
+            print(response.text)
+            return
     
     
     
